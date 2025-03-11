c5094d426c33c9bf6946f9db43d5435ab219ba79
JuleeC <juleshummel44@gmail.com>
2025-03-11 19:34:25 +0100

    twosum

--- twosum_1.java
+++ twosum_1.java
@@ -1,5 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
+        int[] res = new int[2];
+
+        Map<Integer, Integer> map = new HashMap();
+
+        for(int i = 0 ; i < nums.length ; i++) {
+
+
+        }
+
+        return res;
     }
 }
