fad7334fef18bff0eff837f7c2fd49b3c8b235d0
JuleeC <juleshummel44@gmail.com>
2025-03-12 22:27:11 +0100

    just learning :)

--- max_count_of_pos_neg_int_2.java
+++ max_count_of_pos_neg_int_2.java
@@ -3,6 +3,14 @@ class Solution {
         //initialize two vars that count if the int is pos/neg
         int min = 0;
         int max = 0;
+        //we loop through the array
+        for(int i : nums){
+            if(i < 0 ) {//add one to min if i < 0
 
-    }
+                min++;
+            } else  if(i > 0){//add one to min if i > 0
+                max++;
+            }
+        }
+
 }
