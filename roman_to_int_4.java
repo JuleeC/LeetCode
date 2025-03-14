267d8d2fc230c7100d463090f8ddd78a86638f84
JuleeC <juleshummel44@gmail.com>
2025-03-14 14:29:53 +0100

    roman to int

--- roman_to_int_4.java
+++ roman_to_int_4.java
@@ -3,5 +3,26 @@ class Solution {
     int resolution = 0;
     int n = s.length();
 
+      for (int i = 0; i < n; i++) {
+          //get current value from roman number to an integer
+          int currentVal = roman_handler(s.charAt(i));
 
+
+      }
+
+      return resolution;
+  }
+
+    private int roman_handler(char c) {
+        switch(c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
 }
