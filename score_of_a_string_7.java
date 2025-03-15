033b4ce620358f432c16d1f3b83ba6bab9b9471c
JuleeC <juleshummel44@gmail.com>
2025-03-15 17:42:13 +0100

    score of a string

--- score_of_a_string_7.java
+++ score_of_a_string_7.java
@@ -2,6 +2,12 @@ class Solution {
     public int scoreOfString(String s) {
 
         int ans = 0;
+        for(int i = 0; i < s.length()-1; i++)  {
+            if(s == "") return 0;
+            ans += Math.abs((int)s.charAt(i) - (int)s.charAt(i+1));
 
+
+        }
+        return ans;
     }
 }
