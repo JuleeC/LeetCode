c469794e7f41fe72a0b6356a64a0a18b98383f00
JuleeC <juleshummel44@gmail.com>
2025-03-18 20:08:53 +0100

    transferring

--- learning(neetcode)/valid_anagram_2.java
+++ learning(neetcode)/valid_anagram_2.java
@@ -4,6 +4,13 @@ public class Solution {
             return false;
         }
 
-
+        char[] sSort = s.toCharArray();
+        char[] tSort = t.toCharArray();
+        Arrays.sort(sSort);
+        Arrays.sort(tSort);
+        for(int i = 0; i <= sSort.length-1;i++) {
+            if(sSort[i] != tSort[i]) return false;
+        }
+        return true;
     }
 }
