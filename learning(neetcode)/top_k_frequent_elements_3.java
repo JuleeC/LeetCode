23df85491fdc3a0287a4d28f1555d881a76edfa3
JuleeC <juleshummel44@gmail.com>
2025-03-19 21:54:52 +0100

    top k frequent elements

--- learning(neetcode)/top_k_frequent_elements_3.java
+++ learning(neetcode)/top_k_frequent_elements_3.java
@@ -1,6 +1,10 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer,Integer> res = new HashMap<>();
+        Arrays.sort(nums);
+        for(int i : nums) {
+            res.put(i,res.getOrDefault(i,0) +1);
+        }
 
 
     }
