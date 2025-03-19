03af71430e133aeebe1a8fb34de66fba32a05369
JuleeC <juleshummel44@gmail.com>
2025-03-19 21:56:01 +0100

    two sum

--- learning(neetcode)/two_sum_4.java
+++ learning(neetcode)/two_sum_4.java
@@ -1,7 +1,9 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
 
+                }
             }
         }
         return new int[0];
