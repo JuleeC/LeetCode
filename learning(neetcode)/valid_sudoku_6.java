89f9182967cbb2714fdbb2b8da7e6d4bfdf31716
JuleeC <juleshummel44@gmail.com>
2025-03-20 17:08:31 +0100

    valid sudoku

--- learning(neetcode)/valid_sudoku_6.java
+++ learning(neetcode)/valid_sudoku_6.java
@@ -5,7 +5,14 @@ class Solution {
         for(int row = 0; row < 9; row++) {
             Set<Character> seen_row = new HashSet<>();
             //iterate through a row to check if there are duples
+            for(int i = 0; i < 9;i++) {
 
+                if(board[row][i] == '.') continue;
+                if(seen_row.contains(board[row][i])) return false;
+                seen_row.add(board[row][i]);
+            }
+
+        }
 
 
         return true;
