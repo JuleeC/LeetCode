5a61199f36949e74561b8961f8f9124f19530d1f
JuleeC <juleshummel44@gmail.com>
2025-03-21 16:44:54 +0100

    products of array except self

--- learning(neetcode)/products_of_array_except_self_7.java
+++ learning(neetcode)/products_of_array_except_self_7.java
@@ -2,7 +2,10 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
         int dummy;
+        for(int i = 0; i < nums.length; i++) {
 
+            }
+            ans[i] =dummy;
         }
         return ans;
     }
