f52b7b3bea083a861d2bad18c8c7baa12986f3e3
JuleeC <juleshummel44@gmail.com>
2025-03-23 16:31:54 +0100

    longest consecutive sequence

--- learning(neetcode)/longest_consecutive_sequence_9.java
+++ learning(neetcode)/longest_consecutive_sequence_9.java
@@ -2,6 +2,13 @@ public class Solution {
     public int longestConsecutive(int[] nums) {
         int res = 0;
         Set<Integer> store = new HashSet<>();
+        for (int num : nums) {
+            store.add(num);
+        }
 
+        for (int num : nums) {
+
+        }
+        return res;
     }
 }
