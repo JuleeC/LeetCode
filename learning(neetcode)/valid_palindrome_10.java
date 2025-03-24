1b8fcf78b0a7106fa35fe3872cc24d6b562b1317
JuleeC <juleshummel44@gmail.com>
2025-03-24 22:22:11 +0100

    valid palindrome

--- learning(neetcode)/valid_palindrome_10.java
+++ learning(neetcode)/valid_palindrome_10.java
@@ -2,6 +2,10 @@ public class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder newStr = new StringBuilder();
         for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                newStr.append(Character.toLowerCase(c));
+            }
+        }
 
     }
 }
