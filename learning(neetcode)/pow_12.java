a04f3b3386fae470f9b63133a690791f9db09454
JuleeC <juleshummel44@gmail.com>
2025-03-26 17:49:27 +0100

    pow

--- learning(neetcode)/pow_12.java
+++ learning(neetcode)/pow_12.java
@@ -4,6 +4,12 @@ class Solution {
 
         if(n == 0) { return 1;}
 
+        double res = 1;
+        for(int i = 0; i <Math.abs(n); i++) {
+            res *= x;
+        }
+
+        if(n < 0) {return 1/res;}
 
     }
 }
