1d17453c1b7908ae0a08b17226f98a14e9a58e79
JuleeC <juleshummel44@gmail.com>
2025-03-28 15:37:14 +0100

    group anagrams

--- learning(neetcode)/group_anagrams_14.java
+++ learning(neetcode)/group_anagrams_14.java
@@ -2,6 +2,11 @@ public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res = new HashMap<>();
         for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
 
+        }
+        return new ArrayList<>(res.values());
     }
 }
