a80cf9903370d084af85e9d80bb6b07d6bb0fa18
JuleeC <juleshummel44@gmail.com>
2025-03-28 15:37:01 +0100

    group anagrams

--- learning(neetcode)/group_anagrams_14.java
+++ learning(neetcode)/group_anagrams_14.java
@@ -1,5 +1,7 @@
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
 
     }
 }
