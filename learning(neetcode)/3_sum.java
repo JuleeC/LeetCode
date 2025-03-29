6638f335b3ba02516129e5a475277d9d9547a3b5
JuleeC <juleshummel44@gmail.com>
2025-03-29 23:14:13 +0100

    3 sum

--- learning(neetcode)/3_sum.java
+++ learning(neetcode)/3_sum.java
@@ -2,6 +2,11 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Set<List<Integer>> res = new HashSet<>();
         Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++) {
+
+            }
+        }
+        return new ArrayList<>(res);
 
     }
 }
