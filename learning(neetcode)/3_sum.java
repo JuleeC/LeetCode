ae134806e57b3b63f3c063f8216dc5d703b3ffaa
JuleeC <juleshummel44@gmail.com>
2025-03-29 23:14:30 +0100

    3 sum

--- learning(neetcode)/3_sum.java
+++ learning(neetcode)/3_sum.java
@@ -5,7 +5,9 @@ class Solution {
         for(int i = 0; i < nums.length; i++) {
             for(int j = i+1;j < nums.length;j++) {
                 for(int h = j+1; h < nums.length; h++) {
-
+                    if(nums[i] + nums[j] + nums[h] == 0) {
+                        List<Integer> dummy = Arrays.asList(nums[i],nums[j],nums[h]);
+                        res.add(dummy);
                     }
                 }
             }
