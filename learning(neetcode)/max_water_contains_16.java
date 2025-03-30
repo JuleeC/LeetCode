2e9d2b9c43606b6bd5511ecd497d0e5acc6fa1c0
JuleeC <juleshummel44@gmail.com>
2025-03-30 18:49:01 +0200

    maximum of water that can contain the area

--- learning(neetcode)/max_water_contains_16.java
+++ learning(neetcode)/max_water_contains_16.java
@@ -1,7 +1,10 @@
 class Solution {
     public int maxArea(int[] heights) {
         int ans = 0;
+        for(int i = 0; i < heights.length; i++) {
+            for(int j = i+1; j< heights.length; j++) {
 
+            }
         }
         return ans;
     }
