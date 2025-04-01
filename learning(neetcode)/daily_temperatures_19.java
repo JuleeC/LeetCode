2db3ed7549ddf63569b05584dd32564d1bbf7246
JuleeC <juleshummel44@gmail.com>
2025-04-01 19:58:27 +0200

    daily temperatures

--- learning(neetcode)/daily_temperatures_19.java
+++ learning(neetcode)/daily_temperatures_19.java
@@ -5,6 +5,9 @@ public class Solution {
 
         for (int i = 0; i < temperatures.length; i++) {
             while (!stack.isEmpty() && t > stack.peek()[0]) {
+                int[] pair = stack.pop();
+                res[pair[1]] = i - pair[1];
+            }
 
     }
 }
