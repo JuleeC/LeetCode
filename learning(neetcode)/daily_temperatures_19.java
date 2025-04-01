df8fac11f96c6be1abb7011328eae82579e35ab1
JuleeC <juleshummel44@gmail.com>
2025-04-01 19:58:03 +0200

    daily temperatures

--- learning(neetcode)/daily_temperatures_19.java
+++ learning(neetcode)/daily_temperatures_19.java
@@ -1,4 +1,8 @@
 public class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>(); // pair: [temp, index]
 
+
+    }
 }
