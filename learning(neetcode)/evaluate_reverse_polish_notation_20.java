7fb73debc7aaf07cbfe6c9a9718521a9cf886651
JuleeC <juleshummel44@gmail.com>
2025-04-02 22:39:22 +0200

    evaluate reverse polish notation

--- learning(neetcode)/evaluate_reverse_polish_notation_20.java
+++ learning(neetcode)/evaluate_reverse_polish_notation_20.java
@@ -2,7 +2,12 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
         for (String c : tokens) {
+            if (c.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
 
+            } else {
+                stack.push(Integer.parseInt(c));
+            }
         }
         return stack.pop();
     }
