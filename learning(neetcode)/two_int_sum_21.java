0edf4011c8cc593ac972364edd9445e808ad3b33
JuleeC <juleshummel44@gmail.com>
2025-04-03 20:49:36 +0200

    two int sum

--- learning(neetcode)/two_int_sum_21.java
+++ learning(neetcode)/two_int_sum_21.java
@@ -1,6 +1,9 @@
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
 
+        }
+        return new int[0];
     }
 }
