7c2afafeb18383c14703087ffc525b7735f1e9d8
JuleeC <juleshummel44@gmail.com>
2025-04-10 17:47:45 +0200

    trapping rain water

--- learning(neetcode)/trappping_rain_wate_27.java
+++ learning(neetcode)/trappping_rain_wate_27.java
@@ -1,5 +1,11 @@
 public class Solution {
     public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int n = height.length;
+        int res = 0;
+
 
     }
 }
