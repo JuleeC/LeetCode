29abc4ac714bf73771afcd7d43421605ebc77ecb
JuleeC <juleshummel44@gmail.com>
2025-04-14 09:53:36 +0200

    good count triplets

--- problem_solving(leetcode)/good_count_triplets_11.java
+++ problem_solving(leetcode)/good_count_triplets_11.java
@@ -4,6 +4,12 @@ class Solution {
         for(int i = 0; i< arr.length; i++) {
             for(int j = i +1; j < arr.length; j++) {
                 for(int k = j+1; k< arr.length; k++) {
-
+                    if(Math.abs(arr[i] -arr[j]) <= a &&
+                            Math.abs(arr[j] -arr[k]) <= b &&
+                            Math.abs(arr[i] -arr[k]) <= c) { good_triplets++; }
+                }
+            }
+        }
+        return good_triplets;
     }
 }
