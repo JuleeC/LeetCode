22b380d5aaf6ad5ed00a7863e254e0f42e724704
JuleeC <juleshummel44@gmail.com>
2025-04-14 10:09:04 +0200

    merge strings alternatly

--- problem_solving(leetcode)/merge_strings_alternately_12.java
+++ problem_solving(leetcode)/merge_strings_alternately_12.java
@@ -1,4 +1,9 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        int word1_l = word1.length();
+        int word2_l = word2.length();
+        StringBuilder res = new StringBuilder();
 
+
+    }
 }
