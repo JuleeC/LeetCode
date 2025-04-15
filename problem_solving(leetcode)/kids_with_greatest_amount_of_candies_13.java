97352d7e0b428ab95b6cdd4d140e53b9036d628f
JuleeC <juleshummel44@gmail.com>
2025-04-15 12:06:47 +0200

    kids with greatest amount of candies

--- problem_solving(leetcode)/kids_with_greatest_amount_of_candies_13.java
+++ problem_solving(leetcode)/kids_with_greatest_amount_of_candies_13.java
@@ -1,5 +1,8 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        if(candies == null) {
+            return new ArrayList<>();
+        }
 
     }
 }
