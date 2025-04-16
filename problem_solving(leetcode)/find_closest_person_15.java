26e862c2cbc5aa15e4984091b95c47a4ea480d54
JuleeC <juleshummel44@gmail.com>
2025-04-16 11:10:59 +0200

    find closest person

--- problem_solving(leetcode)/find_closest_person_15.java
+++ problem_solving(leetcode)/find_closest_person_15.java
@@ -2,6 +2,8 @@ class Solution {
     public int findClosest(int x, int y, int z) {
         if(Math.abs(z-x) < Math.abs(z-y)) {
             return 1;
+        } else if(Math.abs(z-x) == Math.abs(z-y)) {
+            return 0;
 
     }
 }
