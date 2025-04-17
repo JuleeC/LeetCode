469d1026d94ffdf0f12abdca086bee8d0d1ec4cc
JuleeC <juleshummel44@gmail.com>
2025-04-17 17:57:20 +0200

    count equal and divisable pair in array

--- problem_solving(leetcode)/count_equal_and_divisable_pair_in_array_16.java
+++ problem_solving(leetcode)/count_equal_and_divisable_pair_in_array_16.java
@@ -2,7 +2,10 @@ class Solution {
     public int countPairs(int[] nums, int k) {
         int res = 0;
         for(int i = 0; i < nums.length-1; i++) {
+            for(int j = i+1; j < nums.length; j++) {
 
+                }
+            }
         }
         return res;
     }
