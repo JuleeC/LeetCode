a9428ac9e7a99e974395258fa621e22a2d5a19ea
JuleeC <juleshummel44@gmail.com>
2025-04-18 12:12:33 +0200

    count and say

--- problem_solving(leetcode)/count_and_say_17.java
+++ problem_solving(leetcode)/count_and_say_17.java
@@ -2,4 +2,10 @@ public class Solution {
     public String countAndSay(int n) {
         if (n == 1) return "1";
 
+        String result = "1";
+
+        for (int i = 2; i <= n; i++) {
+
+            
+    }
 }
