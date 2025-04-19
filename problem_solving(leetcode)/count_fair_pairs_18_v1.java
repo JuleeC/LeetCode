c2e92cfd5f5623bb535748c54616dab337ccd7b1
JuleeC <juleshummel44@gmail.com>
2025-04-19 09:45:38 +0200

    count fair pairs

--- problem_solving(leetcode)/count_fair_pairs_18_v1.java
+++ problem_solving(leetcode)/count_fair_pairs_18_v1.java
@@ -1,4 +1,8 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
+        //if 0 <= i < j < n && lower <= nums[i] + nums[j] <= upper
+        long res = 0;
+        int n = nums.length;
 
+    }
 }
