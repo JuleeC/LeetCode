e5cb8116f46e811d55533a256ef637f7eb382010
JuleeC <juleshummel44@gmail.com>
2025-04-19 09:46:17 +0200

    just learning ;)

--- problem_solving(leetcode)/count_fair_pairs_18_v1.java
+++ problem_solving(leetcode)/count_fair_pairs_18_v1.java
@@ -3,6 +3,13 @@ class Solution {
         //if 0 <= i < j < n && lower <= nums[i] + nums[j] <= upper
         long res = 0;
         int n = nums.length;
+        for(int i = 0; i< n; i++ ) {
+            for(int j = i+1;j < n; j++) {
+                if(nums[i] + nums[j] <= upper && nums[i] + nums[j] >= lower) {
+                    res++;
+                }
+            }
+        }
 
     }
 }
