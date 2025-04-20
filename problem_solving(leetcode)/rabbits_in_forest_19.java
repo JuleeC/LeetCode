44022016ccd73c6f51a3c40aac991c946b2b4215
JuleeC <juleshummel44@gmail.com>
2025-04-20 13:43:44 +0200

    rabbits in forest

--- problem_solving(leetcode)/rabbits_in_forest_19.java
+++ problem_solving(leetcode)/rabbits_in_forest_19.java
@@ -8,7 +8,9 @@ class Solution {
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == 0) {
                 res++;
-            }
+            }else if (i == 0 || answers[i] != answers[i - 1] || count == 0) {
+                res += answers[i] + 1;
+                count = answers[i];
             }
         }
         return res;
