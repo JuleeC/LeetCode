3a70a1f3307c450f2ea040fc51e1ecb0aa15cc5a
JuleeC <juleshummel44@gmail.com>
2025-04-21 15:07:23 +0200

    count the hidden sequences

--- problem_solving(leetcode)/count_hidden_sequences_20.java
+++ problem_solving(leetcode)/count_hidden_sequences_20.java
@@ -2,6 +2,7 @@ class Solution {
 
     public int numberOfArrays(int[] differences, int lower, int upper) {
         int x = 0, y = 0, dummy = 0;
+        for (int i : differences) {
+            dummy += i;
 
-    }
 }
