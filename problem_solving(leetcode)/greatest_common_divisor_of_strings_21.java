20f53b3dc8bd37a6046fe59306d7ff9f34dade94
JuleeC <juleshummel44@gmail.com>
2025-04-21 15:20:44 +0200

    greatest divisor of strings

--- problem_solving(leetcode)/greatest_common_divisor_of_strings_21.java
+++ problem_solving(leetcode)/greatest_common_divisor_of_strings_21.java
@@ -3,4 +3,12 @@ class Solution {
         if((str1 +str2).equals(str2+str1)) { }  else return "";
 
 
+        int gcdLength = gcd_checker(str1.length(), str2.length());
+        return str1.substring(0,gcdLength);
+
+    }
+    public int gcd_checker(int s1,int s2) {
+
+
+    }
 }
