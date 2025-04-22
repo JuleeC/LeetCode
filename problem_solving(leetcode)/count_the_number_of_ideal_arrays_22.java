7c335f2e5e9b11046112e9269d716d69e5468a86
JuleeC <juleshummel44@gmail.com>
2025-04-22 14:18:55 +0200

    count the number of ideal arrays

--- problem_solving(leetcode)/count_the_number_of_ideal_arrays_22.java
+++ problem_solving(leetcode)/count_the_number_of_ideal_arrays_22.java
@@ -3,3 +3,6 @@ class Solution {
     int[][] cnt = new int[10001][14];
     int[][] comb = new int[10001][14];
 
+    public Solution() {
+        for (int s = 0; s < 10001; s++) {
+            for (int r = 0; r <= Math.min(s, 13); r++) {
