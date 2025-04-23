219631199cae02d3e90efe3a0463050e7967f76d
JuleeC <juleshummel44@gmail.com>
2025-04-23 18:49:31 +0200

    count largest groups

--- problem_solving(leetcode)/count_largest_group_24.java
+++ problem_solving(leetcode)/count_largest_group_24.java
@@ -5,5 +5,11 @@ class Solution {
         for(int i =1; i <= n; i++) {
             int key = 0;
             int i_zero = i;
+            while(i_zero != 0) {
+                key += i_zero % 10;
+                i_zero = i_zero / 10;
+            }
 
+
+    }
 }
