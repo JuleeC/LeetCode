ed969389d2b53f462568238c7197010ac7dcc7e2
JuleeC <juleshummel44@gmail.com>
2025-04-23 18:49:23 +0200

    count largest groups

--- problem_solving(leetcode)/count_largest_group_24.java
+++ problem_solving(leetcode)/count_largest_group_24.java
@@ -2,5 +2,8 @@ class Solution {
     public int countLargestGroup(int n) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         int max = 0;
+        for(int i =1; i <= n; i++) {
+            int key = 0;
+            int i_zero = i;
 
 }
