80fb382d403a4f84a846e19b46497e65f64fcfa1
JuleeC <juleshummel44@gmail.com>
2025-04-27 14:49:52 +0200

    count subarrrays with length three and a special condition

--- problem_solving(leetcode)/count_subarrays_with_length_three_27.java
+++ problem_solving(leetcode)/count_subarrays_with_length_three_27.java
@@ -2,6 +2,7 @@ class Solution {
     public int countSubarrays(int[] nums) {
         int res = 0;
         for(int i = 0; i+2 < nums.length; i++) {
+            if((nums[i+2] + nums[i]) * 2 == nums[i+1]) {
 
     }
 }
