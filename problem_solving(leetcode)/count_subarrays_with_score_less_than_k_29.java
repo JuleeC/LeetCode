818cad6379ae14d9df179e3679fbc6a8f08d8ce3
JuleeC <juleshummel44@gmail.com>
2025-04-28 19:12:31 +0200

    count subarrays with score less than k

--- problem_solving(leetcode)/count_subarrays_with_score_less_than_k_29.java
+++ problem_solving(leetcode)/count_subarrays_with_score_less_than_k_29.java
@@ -2,5 +2,8 @@ class Solution {
     public long countSubarrays(int[] nums, long k) {
         long count = 0, sum = 0;
         int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
 
+    }
 }
