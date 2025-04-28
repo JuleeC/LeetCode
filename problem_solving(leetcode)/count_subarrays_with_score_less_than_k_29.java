f1e360f29ae2ef1ffa724ea593276370865e9f72
JuleeC <juleshummel44@gmail.com>
2025-04-28 19:12:36 +0200

    count subarrays with score less than k

--- problem_solving(leetcode)/count_subarrays_with_score_less_than_k_29.java
+++ problem_solving(leetcode)/count_subarrays_with_score_less_than_k_29.java
@@ -4,6 +4,9 @@ class Solution {
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
+            while (sum * (right - left + 1) >= k) {
+                sum -= nums[left++];
+            }
 
     }
 }
