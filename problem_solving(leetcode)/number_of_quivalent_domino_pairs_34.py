292da95bc03f70d092ee03737be8e0274cf5b355
JuleeC <juleshummel44@gmail.com>
2025-05-04 18:58:22 +0200

    number of equivalent domino pairs

--- problem_solving(leetcode)/number_of_quivalent_domino_pairs_34.py
+++ problem_solving(leetcode)/number_of_quivalent_domino_pairs_34.py
@@ -9,7 +9,8 @@ class Solution(object):
         for a,b in dominoes:
             dummy = tuple(sorted([a,b]))
             if dummy in seen:
-
+                cnt += seen[dummy]
+                seen[dummy] += 1
             else:
                 seen[dummy] = 1
         return cnt
