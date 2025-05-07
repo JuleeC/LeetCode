89cd8dcc37e668e74ebb61b3b3a82e23f3ee06d1
JuleeC <juleshummel44@gmail.com>
2025-05-07 18:15:12 +0200

    find minimum time to reach the last room

--- problem_solving(leetcode)/find_minimum_time_to_reach_last_room_37.py
+++ problem_solving(leetcode)/find_minimum_time_to_reach_last_room_37.py
@@ -7,3 +7,7 @@ class Solution(object):
         heapq.heappush(minh, (0, 0, 0))
         moveTime[0][0] = 0
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        while minh:
+            currTime, currRow, currCol = heapq.heappop(minh)
+            if currTime >= dp[currRow][currCol]:
+                continue
