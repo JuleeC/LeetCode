db901b4457eea4836ccb13695406aa1f649b350f
JuleeC <juleshummel44@gmail.com>
2025-05-08 17:12:26 +0200

    find minimum time to reach last room

--- problem_solving(leetcode)/find_minimum_time_to_reach_last_room_38.py
+++ problem_solving(leetcode)/find_minimum_time_to_reach_last_room_38.py
@@ -3,3 +3,6 @@ class Solution(object):
         n = len(moveTime)
         m = len(moveTime[0])
         minh = [(0, 0, 0, 1)]
+        heapq.heapify(minh)
+        minTime = set((0, 0))
+        direction = [(1,0), (0,1), (-1,0), (0,-1)]
