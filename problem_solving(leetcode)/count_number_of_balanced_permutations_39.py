740506b3cecf22aa743b2ad0602ba104d6703d86
JuleeC <juleshummel44@gmail.com>
2025-05-09 15:54:55 +0200

    count number of balanced permutations

--- problem_solving(leetcode)/count_number_of_balanced_permutations_39.py
+++ problem_solving(leetcode)/count_number_of_balanced_permutations_39.py
@@ -3,3 +3,6 @@ class Solution(object):
         mod = 10**9+7
         n = len(num)
         total = sum(int(c) for c in num)
+        if total % 2: return 0
+        fact = [1]*(n+1)
+        inv = [1]*(n+1)
