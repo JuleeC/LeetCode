89b6bff84fbf48d56ba484af553db53bc83389fd
JuleeC <juleshummel44@gmail.com>
2025-05-10 14:22:14 +0200

    minimum sum of two arrays after replacing zeros

--- problem_solving(leetcode)/minimum_equal_sum_of_two_arrays_after_replacing_zeros_40.py
+++ problem_solving(leetcode)/minimum_equal_sum_of_two_arrays_after_replacing_zeros_40.py
@@ -17,3 +17,6 @@ class Solution(object):
             return sum1 if sum1 == sum2 else -1
         elif zeros1 == 0:
             return sum1 if sum2 + zeros2 <= sum1 else -1
+        elif zeros2 == 0:
+            return sum2 if sum1 + zeros1 <= sum2 else -1
+
