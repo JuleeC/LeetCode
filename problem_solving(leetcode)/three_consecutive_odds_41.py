131713796c2c7638d3b693c9ea8d336dad485466
JuleeC <juleshummel44@gmail.com>
2025-05-11 18:20:52 +0200

    three consecutive odds

--- problem_solving(leetcode)/three_consecutive_odds_41.py
+++ problem_solving(leetcode)/three_consecutive_odds_41.py
@@ -7,5 +7,5 @@ class Solution(object):
 
         for i in range(len(arr) -2):
             if(arr[i] % 2 == 1 and arr[i+1] % 2 == 1 and arr[i+2] % 2 == 1):
-
+                return True
         return False
