b613521cf1dd802fd4bc554f9dced9864dddabaf
JuleeC <juleshummel44@gmail.com>
2025-05-12 19:41:31 +0200

    finding three digit even nums

--- problem_solving(leetcode)/finding_3_digit_even_num_42.py
+++ problem_solving(leetcode)/finding_3_digit_even_num_42.py
@@ -2,3 +2,6 @@ class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
         nums = set()
         n = len(digits)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
