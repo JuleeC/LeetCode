25d630d11aa2f39b7f25176c48a342564bf6b4f9
JuleeC <juleshummel44@gmail.com>
2025-05-12 19:41:49 +0200

    finding three digit even nums

--- problem_solving(leetcode)/finding_3_digit_even_num_42.py
+++ problem_solving(leetcode)/finding_3_digit_even_num_42.py
@@ -8,3 +8,5 @@ class Solution:
                     if i == j or j == k or i == k:
                         continue
                     num = digits[i] * 100 + digits[j] * 10 + digits[k]
+                    if num >= 100 and num % 2 == 0:
+                        nums.add(num)
