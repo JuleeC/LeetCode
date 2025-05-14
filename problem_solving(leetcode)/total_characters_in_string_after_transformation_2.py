0fc8307495198cd8d074ee81106ef3b8201da057
JuleeC <juleshummel44@gmail.com>
2025-05-14 19:20:37 +0200

    total characters in string after transformation

--- problem_solving(leetcode)/total_characters_in_string_after_transformation_2.py
+++ problem_solving(leetcode)/total_characters_in_string_after_transformation_2.py
@@ -5,3 +5,8 @@ L = 26
 class Mat:
     def __init__(self, copy_from: "Mat" = None) -> None:
         self.a: List[List[int]] = [[0] * L for _ in range(L)]
+        if copy_from:
+            for i in range(L):
+                for j in range(L):
+                    self.a[i][j] = copy_from.a[i][j]
+
