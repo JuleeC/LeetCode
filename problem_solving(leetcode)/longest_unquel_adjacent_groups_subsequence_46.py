a5ba7b183f19b78f98cfa5aa2eaf94caeb251922
JuleeC <juleshummel44@gmail.com>
2025-05-16 19:28:59 +0200

    longest unqeual adjacent groups subsequence

--- problem_solving(leetcode)/longest_unquel_adjacent_groups_subsequence_46.py
+++ problem_solving(leetcode)/longest_unquel_adjacent_groups_subsequence_46.py
@@ -1,3 +1,8 @@
 class Solution:
     def getWordsInLongestSubsequence(
             self, words: List[str], groups: List[int]) -> List[str]:
+        n = len(groups)
+        dp = [1] * n
+        prev_ = [-1] * n
+        max_index = 0
+
