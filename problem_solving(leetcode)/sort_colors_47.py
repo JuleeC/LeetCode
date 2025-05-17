0a679cb5ceaac0332c593f3966e356c0bfd2c354
JuleeC <juleshummel44@gmail.com>
2025-05-17 11:25:31 +0200

    sort colors

--- problem_solving(leetcode)/sort_colors_47.py
+++ problem_solving(leetcode)/sort_colors_47.py
@@ -5,4 +5,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         low = mid = 0
+        high = len(nums) - 1
+        while mid <= high:
+            if nums[mid] == 0:
 
