a5e2f7ed3830595ffeca672e4e40424d0bf593b7
JuleeC <juleshummel44@gmail.com>
2025-05-17 11:25:24 +0200

    sort colors

--- problem_solving(leetcode)/sort_colors_47.py
+++ problem_solving(leetcode)/sort_colors_47.py
@@ -1,2 +1,8 @@
 class Solution(object):
     def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        low = mid = 0
+
