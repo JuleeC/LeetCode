ccc22ba21582a1c59f9c578a569eddeebb92af9e
JuleeC <juleshummel44@gmail.com>
2025-05-18 15:49:39 +0200

    painting a grid with three different colors

--- problem_solving(leetcode)/Painting_a_Grid_With_Three_Different_Colors_48.py
+++ problem_solving(leetcode)/Painting_a_Grid_With_Three_Different_Colors_48.py
@@ -1,2 +1,7 @@
 class Solution(object):
     def colorTheGrid(self, m, n):
+        mod = 10**9+7
+        total = 1
+        for _ in range(m):
+            total *= 3
+        dp = [[0]*total for _ in range(n+1)]
