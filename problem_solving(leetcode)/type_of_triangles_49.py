d1a5b8ca15420714d7dce6754f29af12074b69e5
JuleeC <juleshummel44@gmail.com>
2025-05-19 21:55:12 +0200

    type of triangle

--- problem_solving(leetcode)/type_of_triangles_49.py
+++ problem_solving(leetcode)/type_of_triangles_49.py
@@ -3,3 +3,5 @@ class Solution(object):
         if nums[0] == nums[1] and nums[1] == nums[2]:
             return "equilateral"
         elif (nums[0] == nums[1] or nums[1] == nums[2] or nums[0] == nums[2]) and \
+                nums[0] + nums[1] > nums[2] and nums[1] + nums[2] > nums[0] and nums[0] + nums[2] > nums[1]:
+            return "isosceles"
