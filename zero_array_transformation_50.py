00f027f8eaed5419ba817408073051f6d9744f8a
JuleeC <juleshummel44@gmail.com>
2025-05-20 13:36:47 +0200

    zero array transformation

--- zero_array_transformation_50.py
+++ zero_array_transformation_50.py
@@ -9,3 +9,6 @@ class Solution(object):
         diff = [0] * (len(nums) + 1)
         for l, r in queries:
             diff[l] += 1
+            if r + 1 < len(nums):
+                diff[r + 1] -= 1
+        cnt = 0
