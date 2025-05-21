149d9cf4310c2539bdbd6b0084e079815d4efa3b
JuleeC <juleshummel44@gmail.com>
2025-05-21 19:41:28 +0200

    set matrix zeroes

--- problem_solving(leetcode)/set_zeros_matrix_51.py
+++ problem_solving(leetcode)/set_zeros_matrix_51.py
@@ -3,4 +3,7 @@ class Solution(object):
         first_col = False
         for row in range(len(matrix)):
             if matrix[row][0] == 0:
+                first_col = True
+            for col in range(1, len(matrix[0])):
+                if matrix[row][col] == 0:
 
