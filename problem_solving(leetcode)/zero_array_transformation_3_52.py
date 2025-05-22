9d4bbef2978d1c8276b9f3284a1e3334c86c5310
JuleeC <juleshummel44@gmail.com>
2025-05-22 12:59:13 +0200

    zero array after transformation 3

---
+++ problem_solving(leetcode)/zero_array_transformation_3_52.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maxRemoval(self, nums, queries):
+        """
+        :type nums: List[int]
+        :type queries: List[List[int]]
+        :rtype: int
+        """
