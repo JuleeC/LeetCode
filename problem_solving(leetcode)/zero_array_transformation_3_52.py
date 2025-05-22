1d037be83957f0e19fe0549b298d8ea799950a84
JuleeC <juleshummel44@gmail.com>
2025-05-22 12:59:35 +0200

    zero array after transformation 3

--- problem_solving(leetcode)/zero_array_transformation_3_52.py
+++ problem_solving(leetcode)/zero_array_transformation_3_52.py
@@ -5,3 +5,8 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: int
         """
+
+        queries.sort(key=lambda x: x[0])
+        available = []
+        assigned = []
+        count = 0
