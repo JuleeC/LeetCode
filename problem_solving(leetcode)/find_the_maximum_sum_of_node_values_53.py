13c3cf7a5c1f7cdc016dc9cad7447ef0ac4f46d8
JuleeC <juleshummel44@gmail.com>
2025-05-23 15:04:45 +0200

    find the maximum sum of node values

---
+++ problem_solving(leetcode)/find_the_maximum_sum_of_node_values_53.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maximumValueSum(self, nums, k, edges):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
