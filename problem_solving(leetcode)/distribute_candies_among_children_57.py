7caac9ea1ca81bb7663db92fcd489e20003227fb
JuleeC <juleshummel44@gmail.com>
2025-06-01 13:46:32 +0200

    distribute candies among children

--- problem_solving(leetcode)/distribute_candies_among_children_57.py
+++ problem_solving(leetcode)/distribute_candies_among_children_57.py
@@ -4,3 +4,5 @@ class Solution:
         for i in range(min(limit, n) + 1):
             if n - i > 2 * limit:
                 continue
+            ans += min(n - i, limit) - max(0, n - i - limit) + 1
+        return ans
