d02844b25d6e54ca2f250a83051bd92357ac77f0
JuleeC <juleshummel44@gmail.com>
2025-06-01 11:58:43 +0200

    largest color value

--- problem_solving(leetcode)/largest_color_value_56.py
+++ problem_solving(leetcode)/largest_color_value_56.py
@@ -1,3 +1,12 @@
 
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        n = len(colors)
+        graph = [[] for _ in range(n)]
+        indegree = [0] * n
+
+        for u, v in edges:
+            graph[u].append(v)
+            indegree[v] += 1
+
+        # dp[i][c]: max count of color c on any path ending at node i
