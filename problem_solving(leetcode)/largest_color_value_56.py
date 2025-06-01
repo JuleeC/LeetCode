5046b147fbca824e931d588ed7c3b6e314681094
JuleeC <juleshummel44@gmail.com>
2025-06-01 11:58:48 +0200

    largest color value

--- problem_solving(leetcode)/largest_color_value_56.py
+++ problem_solving(leetcode)/largest_color_value_56.py
@@ -10,3 +10,14 @@ class Solution:
             indegree[v] += 1
 
         # dp[i][c]: max count of color c on any path ending at node i
+        dp = [[0] * 26 for _ in range(n)]
+        queue = deque()
+
+        for i in range(n):
+            if indegree[i] == 0:
+                queue.append(i)
+                dp[i][ord(colors[i]) - ord('a')] = 1
+
+        visited = 0
+        max_color_value = 0
+
