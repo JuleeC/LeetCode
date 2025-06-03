aba19880ad9428d607e3488d949d7835822699a3
JuleeC <juleshummel44@gmail.com>
2025-06-03 21:03:40 +0200

    maximum candies you can get from boxes

---
+++ problem_solving(leetcode)/maximum_candies_you_can_get_from_boxes_59.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxCandies(self, status, candies, keys, containedBoxes, initialBoxes):
+        n = len(status)
+        canOpen = [False] * n
+        hasBox = [False] * n
+        visited = [False] * n
+        queue = []
+
