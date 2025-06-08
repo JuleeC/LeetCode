b2f8071495e05808c96fd9aab4e1c4b1c1cf66cb
JuleeC <juleshummel44@gmail.com>
2025-06-08 12:04:22 +0200

    lexicographicall number

--- problem_solving(leetcode)/lexicographicall_number_64.java
+++ problem_solving(leetcode)/lexicographicall_number_64.java
@@ -3,3 +3,7 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         for (int i = 1; i < 10; i++)
             helper(i, n, ans);
+        return ans;
+    }
+
+    static void helper(int prod, int n, List<Integer> ans) {
