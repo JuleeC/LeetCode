832fc595814d7d35f4835466efb7930a26700a18
JuleeC <juleshummel44@gmail.com>
2025-06-11 15:32:23 +0200

    maximum difference between even and odd frequencies 2

--- problem_solving(leetcode)/maximum_difference_between_even_and_odd_frequences_2.java
+++ problem_solving(leetcode)/maximum_difference_between_even_and_odd_frequences_2.java
@@ -1,4 +1,11 @@
 class Solution {
     public int maxDifference(String s, int k) {
+        int length = s.length();
+        int result = Integer.MIN_VALUE;
+
+        for (int first = 0; first < 5; first++) {
+            for (int second = 0; second < 5; second++) {
+                if (first == second) continue;
+
 
 }
