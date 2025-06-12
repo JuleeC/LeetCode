af059e2863fb4b775eef89318da3effb020d0360
JuleeC <juleshummel44@gmail.com>
2025-06-12 21:04:34 +0200

    maximum difference between adjacent elements in a circular array

--- problem_solving(leetcode)/maximum_difference_between_adjacent_elements_in_a_circular_array_67.java
+++ problem_solving(leetcode)/maximum_difference_between_adjacent_elements_in_a_circular_array_67.java
@@ -2,6 +2,7 @@ class Solution {
     public int maxAdjacentDistance(int[] nums) {
         int maxi = 0;
         for (int i = 0; i < nums.length - 1; i++)
+            maxi = Math.max(maxi, Math.abs(nums[i + 1] - nums[i]));
 
     }
 }
