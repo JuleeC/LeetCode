393a94e02b557e9beabd0bfb93e1d1719cba52d0
JuleeC <juleshummel44@gmail.com>
2025-06-13 15:45:12 +0200

    minimize the maximum difference of pairs

--- problem_solving(leetcode)/minize_the_maximum_difference_of_pairs_68.java
+++ problem_solving(leetcode)/minize_the_maximum_difference_of_pairs_68.java
@@ -2,6 +2,10 @@ class Solution {
     public int minimizeMax(int[] nums, int p) {
         if (p == 0) return 0;
         Arrays.sort(nums);
+        int n = nums.length, left = 0, right = nums[n-1] - nums[0];
+        while (left < right) {
+            int mid = left + (right - left) / 2, pairs = 0;
+            for (int i = 1; i < n; i++) {
 
     }
 }
