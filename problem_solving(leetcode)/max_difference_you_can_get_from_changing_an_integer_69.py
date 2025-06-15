89601ababa3232b2898b2cd1941a2361d5687bb3
JuleeC <juleshummel44@gmail.com>
2025-06-15 10:56:39 +0200

    max difference you can get from changing an integer

--- problem_solving(leetcode)/max_difference_you_can_get_from_changing_an_integer_69.py
+++ problem_solving(leetcode)/max_difference_you_can_get_from_changing_an_integer_69.py
@@ -2,4 +2,7 @@ class Solution:
     def maxDiff(self, num: int) -> int:
         s = str(num)
 
+        match = re.search(r'[^9]', s)
+        maxDigit = match.group() if match else None
+        max = int(s.replace(maxDigit, '9')) if maxDigit else num
 
