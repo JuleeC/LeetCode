9496f6e92aa42f7c0a3541f51dcb6531aa12ad31
JuleeC <juleshummel44@gmail.com>
2025-06-17 17:11:10 +0200

    count the number of arrays with k matching elements

--- problem_solving(leetcode)/count_the_number_of_arrays_with_k_matching_elements_71.java
+++ problem_solving(leetcode)/count_the_number_of_arrays_with_k_matching_elements_71.java
@@ -1,4 +1,8 @@
 class Solution {
     public int countGoodArrays(int n, int m, int k) {
+        if (fact[0] == 0)
+            fact[0] = 1;
+        long res = m * fastExp(m - 1, n - 1 - k) % MOD * combination(n - 1, n - 1 - k) % MOD;
+        return (int) res;
 
 }
