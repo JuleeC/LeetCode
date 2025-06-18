ca52ff23e0f5afe03ccf1b06ced754ea68d0bad1
JuleeC <juleshummel44@gmail.com>
2025-06-18 13:46:29 +0200

    divide arrays into arrays with max differences

--- problem_solving(leetcode)/divide_arrays_into_arrays_with_max_difference_72.java
+++ problem_solving(leetcode)/divide_arrays_into_arrays_with_max_difference_72.java
@@ -3,3 +3,6 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
 
+        int[][] arr = new int[n / 3][3];
+        for (int i = 0; i < n; i += 3) {
+            if (nums[i + 2] - nums[i] > k) return new int[0][];
