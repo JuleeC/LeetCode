486a1daff5f943ea9858a8ec1999823878e11eee
JuleeC <juleshummel44@gmail.com>
2025-06-21 13:06:26 +0200

    minimum deletions to make string k special

--- problem_solving(leetcode)/minimum_deletions_to_make_k_special_75.java
+++ problem_solving(leetcode)/minimum_deletions_to_make_k_special_75.java
@@ -1,2 +1,7 @@
 class Solution {
     public int minimumDeletions(String word, int k) {
+        int[] freq = new int[26];
+        for (int i = 0; i < word.length(); i++) {
+            freq[word.charAt(i) - 'a']++;
+        }
+
