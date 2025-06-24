6e977d839ecd39f93c01a1746cd6d5513d2f9f47
JuleeC <juleshummel44@gmail.com>
2025-06-24 14:10:55 +0200

    find all k distant indicees

--- problem_solving(leetcode)/find_all_k_distant_indices_78.java
+++ problem_solving(leetcode)/find_all_k_distant_indices_78.java
@@ -1,3 +1,7 @@
 import java.util.*;
 
 class Solution {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        int right = 0;
