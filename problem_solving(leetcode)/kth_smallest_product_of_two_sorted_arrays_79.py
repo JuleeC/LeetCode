b7e4bbde693f1a8aaa8a9e713aeca78e8253ed12
JuleeC <juleshummel44@gmail.com>
2025-06-25 13:51:01 +0200

    kth smalles product of two sorted arrays

--- problem_solving(leetcode)/kth_smallest_product_of_two_sorted_arrays_79.py
+++ problem_solving(leetcode)/kth_smallest_product_of_two_sorted_arrays_79.py
@@ -1,2 +1,8 @@
 class Solution:
     def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        nums1.sort()
+        nums2.sort()
+
+        def count_pairs(x: int) -> int:
+            count = 0
+            for a in nums1:
