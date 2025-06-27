ab4163cb647a0eed42d6c24b15fc7d367a1c6816
JuleeC <juleshummel44@gmail.com>
2025-06-27 11:29:47 +0200

    longest subsequence of k times

--- problem_solving(leetcode)/longest_subsequence_of_k_times_81.java
+++ problem_solving(leetcode)/longest_subsequence_of_k_times_81.java
@@ -2,3 +2,9 @@ class Solution {
     public String longestSubsequenceRepeatedK(String s, int k)
     {
 
+        char[] chars = s.toCharArray();
+        int len = chars.length;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < len; i++) {
+            freq[chars[i] - 'a']++;
