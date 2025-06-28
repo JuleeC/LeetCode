24630d356379acbe17d462322b4cb3edebdd09c8
JuleeC <juleshummel44@gmail.com>
2025-06-28 17:39:48 +0200

    find subsequence of length k with the largest sum

--- problem_solving(leetcode)/find_subsequence_of_length_k_with_the_largest_sum_82.py
+++ problem_solving(leetcode)/find_subsequence_of_length_k_with_the_largest_sum_82.py
@@ -1,2 +1,5 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        # Step 1: Keep track of original indices
+        indexed = [(num, i) for i, num in enumerate(nums)]
+
