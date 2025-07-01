1be6aca5f02ee4970f1c97245501c6468ecd39f1
JuleeC <juleshummel44@gmail.com>
2025-07-01 11:56:35 +0000

    find the largest typed string

--- problem_solving(leetcode)/find_the_original_typed_string_85.java
+++ problem_solving(leetcode)/find_the_original_typed_string_85.java
@@ -3,5 +3,7 @@ class Solution {
         int length = word.length();
         int extras = 0;
         for (int i = 1; i < length; i++) {
-   }
+            if (word.charAt(i - 1) == word.charAt(i)) extras++;
+        }
+    }
 }
