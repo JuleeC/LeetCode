bdc38d85343e2e6d3dcea258b8301b614db4716a
JuleeC <juleshummel44@gmail.com>
2025-07-03 12:18:12 +0000

    find the kth character in string game

--- problem_solving(leetcode)/find_the_kth_character_in_string_game_1_87.java
+++ problem_solving(leetcode)/find_the_kth_character_in_string_game_1_87.java
@@ -2,5 +2,7 @@ class Solution {
     public char kthCharacter(int k) {
         StringBuilder sb = new StringBuilder("a");
         while (sb.length() < k) {
+            int size = sb.length();
+            for (int i = 0; i < size; i++) {
    }
 }
